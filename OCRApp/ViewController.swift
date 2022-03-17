0297e4b3aea0872eab3b4bf6721b85ec7af406d9
Gopal <mitsind749@acsicorp.com>
2022-03-17 21:21:48 +0530

    developer print

--- OCRApp/ViewController.swift
+++ OCRApp/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Gopal Working Fine Now")
+        print("Gopala develoepr Branch")
         // Do any additional setup after loading the view.
     }
 
